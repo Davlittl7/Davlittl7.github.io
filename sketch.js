b2fd0efc90459cac33228c526436cf2b35f1870a
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-04-03 21:36:57 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let rects = []
+function setup() {
+  createCanvas(600, 600);
+  background(220);
+  textSize(25);
+  text("Draw Randomly Sized and Colored Rectangles", 50, 100);
+  text("With a click of the mouse!", 150, 150);
+}
+
+function draw() {
+  
+}
+
+
+/*
+function randomRectangles() {
+  if(mouseIsPressed()) {
+    rect(mouseX, mouseY, random(50), random(50));
+  }
+}
+*/
+
+function mousePressed() {
+  fill(random(255), random(255), random(255));
+  rect(mouseX, mouseY, random(100), random(100));
+  return false;
+}
